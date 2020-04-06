2f720961de32f0f1a1ee5ad904c90067d48f3be6
Aloïs Micard <alois@micard.lu>
2020-04-06 16:45:57 +0200

    Reduce docker image compile time

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# build processes
 docker build . -f build/Dockerfile-crawler -t trandoshan.io/crawler
 docker build . -f build/Dockerfile-feeder -t trandoshan.io/feeder
 docker build . -f build/Dockerfile-scheduler -t trandoshan.io/scheduler
