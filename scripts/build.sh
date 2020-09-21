589f3e2b07e05f5f4e4fc7b8e4b1840f2887e3f0
Aloïs Micard <alois@micard.lu>
2020-09-21 06:40:04 +0200

    Use dockerhub for images.
    Closes: #22

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@
 # build docker images
 for path in build/docker/Dockerfile-*; do
   name=$(echo "$path" | cut -d'-' -f2)
-  docker build . -f "$path" -t "trandoshan.io/$name"
+  docker build . -f "$path" -t "creekorful/tdsh-$name"
 done
